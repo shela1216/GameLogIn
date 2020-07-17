345409053f060a781c30b98ece02ef484459fb6a
shela1216 <shela1216@gmail.com>
2020-07-17 15:20:14 +0800

    no message

--- js/index.js
+++ js/index.js
@@ -66,7 +66,7 @@
             if (this.args.groupId && this.args.userId) {
                 this.groupId = this.args.groupId;
                 this.userId = this.args.userId;
-                this.lineName = b64.decode64(this.args.UserName)
+                this.lineName = BASE64.decode(this.args.UserName)
                 this.AllowLogIn = true;
                 var self = this;
                 var hadInfo =false;
