5ec2b9f7bbe9d4bf46b4276f4b30d8cef0111eff
shela1216 <shela1216@gmail.com>
2020-07-18 09:00:00 +0800

    修正同userId覆蓋簽到紀錄問題

--- js/index.js
+++ js/index.js
@@ -117,7 +117,7 @@
 
                     today.getHours() + ':' + today.getMinutes();
                 if (this.isFirst) {
-                    var setDoc = ref.doc(self.userId);
+                    var setDoc = ref.doc(self.userId+self.groupid);
                     setDoc.set({
                         gameUser: self.gameName,
                         gameWork: self.work,
@@ -131,7 +131,7 @@
                         window.location.reload();
                     });
                 } else {
-                    var setDoc = ref.doc(self.userId);
+                    var setDoc = ref.doc(self.userId+self.groupid);
                     setDoc.get().then(doc => {
                         var data = doc.data()
                         if (data['password'] != self.password) {
